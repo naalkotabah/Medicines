0ff0e794d7163680a1f511b8900137ffaa22c270
abbas-alkhazrajy <abbas.mohammed.aziz.kz@gmail.com>
2025-02-02 13:30:11 +0300

    Dto

---
+++ Medicines/Data/dto/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿using Medicines.Data.Models;
+
+namespace Medicines.Data.dto
+{
+    public class OrderDto
+    {
+        public byte Age { get; set; }
+        public float Weight { get; set; }
+        public string ChronicDiseases { get; set; }
+        public string DrugAllergy { get; set; }
+        public string Address { get; set; }
+        public int UserId { get; set; }
+        public int PharmacyId { get; set; }
+        public List<OrderMedicine> OrderMedicines { get; set; }
+    }
+
+}
